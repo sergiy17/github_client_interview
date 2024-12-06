fffd3fb500f6529d2f27039cd9901d89e571e660
Sergiy Parkhuts <sergiy.parkhuts@andopen.co>
2024-12-06 14:13:09 +0200

    The solution

--- client.rb
+++ client.rb
@@ -2,35 +2,32 @@ require 'httparty'
 
 module Github
     class Client
-        # this class is responsible for making requests to the Github API
-        # It accepts a personal access token and stores it as an instance variable.
-        # It has a method called `get` that accepts a URL and returns the response
-        # from the Github API
-
         def initialize(token, repo_url)
-            # implement this method
             @token = token
             @repo_url = repo_url
         end
 
-        def get(url)
-        # this method generates the required headers that use the bearer token 
-        # and makes a GET request to the Github API using the provided URL.
-        # It returns the response from the Github API
-        # It appends the path in the url argument to the repo_url instance variable
-        # to form the full URL
-            HTTParty.get("#{@repo_url}#{url}", headers: headers)
-        end
-
-        private
-
-
-        def headers
-            # this method returns the headers required to make requests to the Github API using a personal access token
-            {
-                'Authorization' => "Bearer #{@token}",
-                'User-Agent' => 'Github Client'
+        def get(path: nil, page: 1, per_page: 50)
+            headers = {
+              'Authorization' => "Bearer #{@token}",
+              'User-Agent' => 'Github Client'
             }
+
+            response = HTTParty.get("#{@repo_url}#{path}", headers: headers, query: { page: page, per_page: per_page, state: 'closed' })
+
+            if response.success?
+                records = JSON.parse(response.body)
+
+                more_records_available = response.headers['Link']&.match(/<(.*)>; rel="next"/)&.captures&.any?
+                if more_records_available
+                    records += get(path: path, page: page += 1, per_page: per_page)
+                end
+
+                records
+            else
+                raise "Error fetching issues: #{response.code} #{response.message}"
+            end
         end
     end
+
 end
--- process.rb
+++ process.rb
@@ -1,30 +1,17 @@
 require_relative './client.rb'
 require 'json'
+require 'debug'
 
 module Github
     class Processor
-        # This class is responsible for processing the response from the Github API.
-        # It accepts a client object and stores it as an instance variable.
-        # It has a method called `issues` that returns a list of issues from the Github API.
         def initialize(client)
             @client = client
         end
 
         def issues(open: true)
-            # This method returns a list of issues from the Github API.
-            # It accepts an optional argument called `open` that defaults to true.
-            # If `open` is true, it returns only open issues.
-            # If `open` is false, it returns only closed issues.
-            # It makes a GET request to the Github API using the client object.
-            # It returns the response from the Github API.
-
             state = open ? 'open' : 'closed'
-             # Return a list of issues from the response, with each line showing the issue's title, whether it is open or closed,
-            # and the date the issue was closed if it is closed, or the date the issue was created if it is open.
-            # the issues are sorted by the date they were closed or created, from newest to oldest.
-            
-            response = @client.get("/issues?state=#{state}")
-            issues = JSON.parse(response.body)
+            issues = @client.get(path: "/issues?state=#{state}")
+
             sorted_issues = issues.sort_by do |issue|
                 if state == 'closed'
                     issue['closed_at']
@@ -32,13 +19,13 @@ module Github
                     issue['created_at']
                 end
             end.reverse
-           
+
             sorted_issues.each do |issue|
-              if issue['state'] == 'closed'
-                puts "#{issue['title']} - #{issue['state']} - Closed at: #{issue['closed_at']}"
-              else
-                puts "#{issue['title']} - #{issue['state']} - Created at: #{issue['created_at']}"
-              end
+                if issue['state'] == 'closed'
+                    puts "#{issue['title']} - #{issue['state']} - Closed at: #{issue['closed_at']}"
+                else
+                    puts "#{issue['title']} - #{issue['state']} - Created at: #{issue['created_at']}"
+                end
             end
         end
     end
